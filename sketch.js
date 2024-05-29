60d2c23d0bba89ce82243da0707910b62d24cdc3
slipknot <00001099287790sp@al.educacao.sp.gov.br>
2024-05-29 14:10:12 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,41 @@
+let cor;
+let posicaoHorizontal; // x
+let posicaoVertical; // y
+
 function setup() {
-  createCanvas(600, 600);
-   background("white");
+  createCanvas(400, 400);
+   background(color(100,0,0));
+  cor = color(random(0,255), random(0,255), random(0,255));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
 }
 
 
 function draw() {
   
-  stroke ("blue");
-  fill ("red");
+  fill(cor);
+  circle(posicaoHorizontal,posicaoVertical,50);
  
   
-  // console.log(mouseIsPressed);
+  
+  if (mouseX < posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal - 1;
+  }
+  
+  if (mouseX > posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal + 1;
+  }
+  
+  if (mouseY < posicaoVertical){
+    posicaoVertical--;
+  }
+    
+  if (mouseY > posicaoVertical){
+    posicaoVertical++;
+  }
   
   if (mouseIsPressed){
-    rect(mouseX, mouseY, 20, 35);
+    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
   }
+    
 }
